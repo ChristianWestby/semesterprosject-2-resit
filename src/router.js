410299eb355de53bf13b354187fadb0c050fb2e0
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2025-04-07 00:35:01 +0200

    Added login route to router.js

--- src/router.js
+++ src/router.js
@@ -1,16 +1,17 @@
 import { setupHome } from './pages/home.js';
 import { setupPetList } from './pages/productList.js';
 import { setupSingleProduct } from './pages/singleProduct.js';
+import { setupLogin } from './admin/login.js';
 
 const app = document.getElementById('app');
-
-// Finn hvilken side vi er på (basert på path)
 const path = window.location.pathname;
 
 if (path === '/' || path.includes('home') || path.includes('index.html')) {
-    setupHome(app);
+  setupHome(app);
 } else if (path.includes('/pet/index.html')) {
   setupSingleProduct(app);
+} else if (path.includes('/account/login.html')) {
+  setupLogin(app); 
 } else if (path.includes('/index.html')) {
   setupPetList(app);
 } else {
