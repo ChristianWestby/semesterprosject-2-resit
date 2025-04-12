6fb90577f184235413daf446946f99951d3525ee
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2025-04-12 16:40:55 +0200

    worked on router, auth and login

---
+++ src/utils/auth.js
@@ -0,0 +1,25 @@
+const TOKEN_KEY = 'authToken';
+
+export function saveToken(token) {
+  localStorage.setItem(TOKEN_KEY, token);
+}
+
+export function getToken() {
+  return localStorage.getItem(TOKEN_KEY);
+}
+
+export function removeToken() {
+  localStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem("name");
+  localStorage.removeItem("email");
+}
+
+export function isLoggedIn() {
+  return !!getToken();
+}
+
+// 👇 Legg til dette nederst
+export function saveUserInfo({ name, email }) {
+  localStorage.setItem("name", name);
+  localStorage.setItem("email", email);
+}
